96617b14dea080964f67a40df37f6dbaff148d20
Wadah Dahi <waddahdahi22@gmail.com>
2025-10-22 03:34:05 -0700

    style: added prefers-color-scheme rule to force light mode and avoid dark mode issues

--- src/scripts/scripts.tsx
+++
@@ -1,9 +0,0 @@
-// SCROLL TO MIDDLE WHEN CLICK
-const handleCardClick = (e: React.MouseEvent<HTMLDivElement>) => {
-  e.currentTarget.scrollIntoView({
-    behavior: "smooth",
-    block: "center",
-    inline: "center",
-  });
-};
-export default handleCardClick;
